3fc04d20008a4e7c2882e8dc95c5bad7b6ecef69
hira <hquddus021@gmail.com>
2020-06-23 20:29:26 -0400

    starwars and flame done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./styles/index.scss";
+import "./styles/reset.scss";
+// import anime from 'animejs/lib/anime.es.js';
+// const anime = require('animejs');
+
+document.addEventListener("keydown", (e) => {
+    
+        console.log(e)
+           
+        if (e.keyCode === 70) {
+            let audio = document.getElementById("audio");
+            audio.playbackRate = audio.playbackRate + 0.1;
+            console.log(audio.playbackRate)
+
+        } else if (e.keyCode === 83) {
+           
+            console.log(e.keycode)
+            let audio = document.getElementById("audio");
+            audio.playbackRate = audio.playbackRate - 0.1;
+        }
+
+})
+
+document.getElementById('sound-3').addEventListener('mouseover', init);
